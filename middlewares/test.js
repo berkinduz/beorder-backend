7199e8d9b26d391ac2f659a4bacc2243a60b1d3c
Berkin <duzberkin@gmail.com>
2021-03-14 17:43:01 +0300

    Test middlewaresi'ne açıklama eklendi

--- middlewares/test.js
+++ middlewares/test.js
@@ -1,3 +1,7 @@
+// Midlleware örneği, test fonksiyonundaki koşul sağlanması durumunda hata döndürür
+// inde.js'de bir route'ye atanırsa route bu fonksiyona göre ya hata verir ya da 
+// test fonksiyonundaki next() dönüşü ile çalışmaya devam eder.
+
 let test = (req, res, next) => {
   if (2 === 1) {
     return res.send("You are not allowed, get out.");
