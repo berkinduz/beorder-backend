d8fac0644c8f1e134bc35e72f2b9d394922789ea
Berkin <duzberkin@gmail.com>
2021-03-22 18:34:43 +0300

    Material create oluşturuldu

--- queries/material.js
+++ queries/material.js
@@ -1,4 +1,5 @@
 const pool = require("../config");
+const jwt = require("jsonwebtoken");
 
 const getMaterials = (request, response) => {
   pool.query("SELECT * FROM materials ORDER BY id ASC", (error, results) => {
@@ -12,15 +13,42 @@ const getMaterials = (request, response) => {
 const getMaterialById = (request, response) => {
   const id = parseInt(request.params.id);
 
-  pool.query("SELECT * FROM materials WHERE id = $1", [id], (error, results) => {
-    if (error) {
-      throw error;
+  pool.query(
+    "SELECT * FROM materials WHERE id = $1",
+    [id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).json(results.rows);
     }
-    response.status(200).json(results.rows);
-  });
+  );
+};
+
+const createMaterial = (request, response) => {
+  const { name, allergen, onStock, price, status } = request.body;
+
+  const token = request.cookies.u_auth;
+  const owner_user_id = jwt.decode(token, "asd");
+
+  pool.query(
+    "INSERT INTO materials (name,allergen,onstock,price,status, owner_user_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id",
+    [name, allergen, onStock, price, status, owner_user_id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response
+        .status(201)
+        .send(
+          `Material added with ID:${results.rows[0].id} by user ID: ${owner_user_id} `
+        );
+    }
+  );
 };
 
 module.exports = {
   getMaterials,
-  getMaterialById
+  getMaterialById,
+  createMaterial,
 };
