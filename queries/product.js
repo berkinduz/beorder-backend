46b1d22324e1ef4593fdc3ae0c96f1c7001989f9
Berkin <duzberkin@gmail.com>
2021-03-28 18:05:39 +0300

    Product post modeli yaratıldı

--- queries/product.js
+++ queries/product.js
@@ -13,19 +13,44 @@ const getProduct = (request, response) => {
 const getProductById = (request, response) => {
   const id = parseInt(request.params.id);
 
+  pool.query("SELECT * FROM product WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+const createProduct = (request, response) => {
+  const { 
+    name, 
+    onStock, 
+    productType, 
+    price, 
+    status, 
+    material } = request.body;
+
+  const token = request.cookies.u_auth;
+  const owner_user_id = jwt.decode(token, "asd");
+
   pool.query(
-    "SELECT * FROM product WHERE id = $1",
-    [id],
+    "INSERT INTO product (name,onstock,product_type,price,status, owner_user_id, material) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id",
+    [name, onStock, productType, price, status, owner_user_id, material],
     (error, results) => {
       if (error) {
         throw error;
       }
-      response.status(200).json(results.rows);
+      response
+        .status(201)
+        .send(
+          `Product added with ID:${results.rows[0].id} by user ID: ${owner_user_id} `
+        );
     }
   );
 };
 
 module.exports = {
   getProduct,
-  getProductById
+  getProductById,
+  createProduct
 };
